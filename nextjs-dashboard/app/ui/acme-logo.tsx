9475b716e572c0b44fcf5f14d5e9014bec3cec4f
Andrew Blair <Andrew.Blair@dcthomson.co.uk>
2024-09-06 11:14:44 +0100

    initial commit

---
+++ nextjs-dashboard/app/ui/acme-logo.tsx
@@ -0,0 +1,20 @@
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
+      <Image
+        src="/logo.png"
+        width="150"
+        height="12"
+        className="hidden md:block"
+        alt="DC Thomson Logo"
+      />
+      <p className="text-[44px]">EIS</p>
+    </div>
+  );
+}
